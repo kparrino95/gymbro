efb3c794d8c0aa7ca778f8cf8564257c1d5747e8
kevin.parrino <kevin.parrino@addvalue.it>
2024-11-09 15:29:18 +0100

    Added configuration for surrealdb connection

--- gymbro-backend/src/index.js
+++ gymbro-backend/src/index.js
@@ -1,4 +1,7 @@
 const server = require('../src/config/server');
+const { connectDB } = require('../src/config/database');
+
+connectDB();
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`);
